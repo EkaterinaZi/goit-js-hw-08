0e6376a10b3aaa38c86e6bfc3e0ecb2af13da7e3
EkaterinaZi <kat.zinovik@gmail.com>
2022-07-21 00:14:47 +0100

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
 import Vimeo from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
+
     const player = new Vimeo.Player(iframe);
 
     player.on('play', function() {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import trottle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('submit', onSubmitedForm)
+form.addEventListener('input', trottle(
+e =>{
+    formData[e.target.name] = e.target.value
+    console.log(formData)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}), 500)
+
+populateForm();
+
+
+function onSubmitedForm (e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(localStorage.getItem(STORAGE_KEY));
+    localStorage.removeItem(STORAGE_KEY);
+    
+}
+
+function populateForm(e){
+    const savedStorage = localStorage.getItem(STORAGE_KEY)
+    if(savedStorage){
+        const savedForm = JSON.parse(savedStorage);
+       ////////як вставити масив в інпут
+    
+    }
+   
+}
